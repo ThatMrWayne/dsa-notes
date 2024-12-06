e33dddbe1711a9f402ef7dceb5f5358f53f4c152
Wayne Hsu <71208879+ThatMrWayne@users.noreply.github.com>
2024-12-06 11:18:13 +0800

    Update: update 380 on 2024-12-06

--- problem_practice/insert_delete_getrandom.py
+++ problem_practice/insert_delete_getrandom.py
@@ -1,31 +1,30 @@
 import random
-# 274
+# 380
 
 
 class RandomizedSet:
 
     def __init__(self):
         self.data_dict = dict()
-        self.idx_dict = dict() # for getrandom
-        self.curr_idx = None
+        self.data_arr = [] # for getrandom
 
     def insert(self, val: int) -> bool:
         if val in self.data_dict:
             return False
-        temp_idx = self.curr_idx+1 if self.curr_idx is not None else 0
-        self.data_dict[val] = temp_idx
-        self.idx_dict[temp_idx] = val
-        self.curr_idx = temp_idx
+        self.data_dict[val] = len(self.data_arr)
+        self.data_arr.append(val)
         return True
 
     def remove(self, val: int) -> bool:
         if val not in self.data_dict:
             return False
-        temp_idx = self.data_dict[val]
+        rm_idx = self.data_dict[val]
+        last_item = self.data_arr[-1]
+        self.data_arr[rm_idx], self.data_arr[-1] = self.data_arr[-1], self.data_arr[rm_idx]
+        self.data_arr.pop()
+        self.data_dict[last_item] = rm_idx
         del self.data_dict[val]
-        del self.idx_dict[temp_idx]
         return True
 
     def getRandom(self) -> int:
-        random_idx = random.choice(list(self.idx_dict.keys()))
-        return self.idx_dict[random_idx]
+        return random.choice(self.data_arr)
