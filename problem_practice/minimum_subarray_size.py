c985162942fa0a528f308574f5a20d1aa9d049f3
Wayne Hsu <71208879+ThatMrWayne@users.noreply.github.com>
2024-12-22 20:09:39 +0800

    Update: on 2024-12-22

---
+++ problem_practice/minimum_subarray_size.py
@@ -0,0 +1,15 @@
+#209
+# TC: O(n)
+# SC: O(1)
+
+
+def minSubArrayLen(target, nums):
+    l, total = 0, 0
+    res = float('inf')
+    for r in range(len(nums)):
+        total += nums[r]
+        while total >= target:
+            res = min(r-l+1, res)
+            total -= nums[l]
+            l+=1
+    return 0 if res == float('inf') else res
