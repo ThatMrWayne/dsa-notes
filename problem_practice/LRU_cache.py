7e180697ef80637968f29b512ff927bb9892b1c8
Wayne Hsu <71208879+ThatMrWayne@users.noreply.github.com>
2024-12-24 10:42:04 +0800

    Update: on 2024-12-24

--- problem_practice/LRU_cache.py
+++ problem_practice/LRU_cache.py
@@ -3,45 +3,53 @@
 
 class Node:
     def __init__(self, key, val):
-        self.key=key
-        self.val=val
-        self.prev=None
-        self.next=None
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
 
 class LRUCache:
-
     def __init__(self, capacity: int):
-        self.size = 0
         self.capacity = capacity
+        self.size = 0
         self.map = {}
         self.head = None
         self.tail = None
 
-    def _update_node_position_if_not_tail(self, existed_node):
-        prev_node = existed_node.prev
+    def _append_to_tail(self, node):
+        self.tail.next = node
+        node.prev = self.tail
+        self.tail = node
+
+    def _delete_head(self):
+        temp_head = self.head.next
+        self.head.next = None
+        temp_head.prev = None
+        del self.map[self.head.key]
+        self.head = temp_head
+
+    def _update_non_tail_node_position(self, existed_node):
         next_node = existed_node.next
-        if prev_node is not None:
+        prev_node = existed_node.prev
+        if existed_node is self.head:
+            temp_head = existed_node.next
+            existed_node.next = None
+            temp_head.prev = None
+            self.head = temp_head
+        else:
             prev_node.next = next_node
             next_node.prev = prev_node
+            existed_node.next = None
             existed_node.prev = None
-            existed_node.next = None
-        else:
-            existed_node.next = None
-            next_node.prev = None
-            self.head = next_node
-        self.tail.next = existed_node
-        existed_node.prev = self.tail
-        self.tail = existed_node 
-
+        self._append_to_tail(existed_node)
 
     def get(self, key: int) -> int:
         if key not in self.map:
             return -1
-        else:
-            tar_node = self.map[key]
-            if tar_node is not self.tail:
-                self._update_node_position_if_not_tail(tar_node)
-            return tar_node.val
+        node = self.map[key]
+        if node is not self.tail:
+            self._update_non_tail_node_position(node)
+        return node.val
 
     def put(self, key: int, value: int) -> None:
         if key not in self.map:
@@ -52,22 +60,13 @@ class LRUCache:
                 if self.tail is None:
                     self.head = self.tail = new_node
                 else:
-                    self.tail.next = new_node
-                    new_node.prev = self.tail
-                    self.tail = new_node
+                    self._append_to_tail(new_node)
                 self.size += 1
-            else: #滿了
-                self.tail.next = new_node
-                new_node.prev = self.tail
-                self.tail = new_node
-                temp_head_next = self.head.next
-                self.head.next = None
-                temp_head_next.prev = None
-                head_key = self.head.key
-                del self.map[head_key]
-                self.head = temp_head_next
+            else:  #滿
+                self._append_to_tail(new_node)
+                self._delete_head()
         else:
             existed_node = self.map[key]
             existed_node.val = value
             if existed_node is not self.tail:
-                self._update_node_position_if_not_tail(existed_node)
+                self._update_non_tail_node_position(existed_node)
